d4e2eb1b0dc2b1a730f999588d221c6634a47e36
helloloop <1204386811@qq.com>
2023-05-29 16:58:50 -0600

    111

--- src/views/Mission.vue
+++ src/views/Mission.vue
@@ -74,13 +74,28 @@
     </template>
 
     <el-form-item>
-      <el-button type="primary" @click="submitForm(formRef)"
+      <el-button type="primary" @click="preview(formRef)"
         >Preview Mission Route</el-button
       >
-      <el-button @click="addPosition">New domain</el-button>
+      <el-button @click="addPosition">New Position</el-button>
       <el-button @click="resetForm(formRef)">Reset</el-button>
     </el-form-item>
   </el-form>
+
+  <el-dialog
+    v-model="centerDialogVisible"
+    style="height: 80vh"
+    width="80%"
+    align-center
+  >
+    <GMapMap :center="location" :zoom="18" style="width: 100%; height: 500px">
+      <GMapMarker :position="location" :clickable="true" />
+      <GMapPolygon :paths="paths" />
+    </GMapMap>
+    <el-button @click="uploadMission" type="primary"
+      >Upload This Flight Mission to UAV</el-button
+    >
+  </el-dialog>
 </template>
 
 <script lang="ts" setup>
@@ -88,6 +103,7 @@ import { reactive, ref } from "vue";
 import type { FormInstance } from "element-plus";
 import { onMounted } from "vue";
 import axios from "axios";
+
 const formRef = ref<FormInstance>();
 const form = reactive<{
   positions: PositionItem[];
@@ -102,8 +118,7 @@ const form = reactive<{
   ],
 });
 
-const current: PositionItem = reactive({
-  key: 0,
+const current = reactive({
   latitude: "",
   longitude: "",
   altitude: "",
@@ -125,20 +140,29 @@ const removePosition = (item: PositionItem) => {
   }
 };
 
+const centerDialogVisible = ref(false);
 const addPosition = () => {
   form.positions.push({
     key: Date.now(),
-    latitude: "",
-    longitude: "",
-    altitude: "",
+    latitude: current.latitude,
+    longitude: current.longitude,
+    altitude: current.altitude,
   });
 };
 
-const submitForm = (formEl: FormInstance | undefined) => {
+const preview = (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   formEl.validate((valid) => {
     if (valid) {
-      console.log("submit!");
+      paths.value = [location];
+      for (let i of form.positions) {
+        paths.value.push({
+          lat: Number(i.latitude),
+          lng: Number(i.longitude),
+        });
+      }
+
+      centerDialogVisible.value = true;
     } else {
       console.log("error submit!");
       return false;
@@ -151,12 +175,44 @@ const resetForm = (formEl: FormInstance | undefined) => {
   formEl.resetFields();
 };
 
+const location = reactive({
+  lat: 1,
+  lng: 1,
+});
+const paths = ref([
+  {
+    lat: 1,
+    lng: 1,
+  },
+]);
+
+const uploadMission = async () => {
+  let command = [];
+
+  for (let i of form.positions) {
+    command.push({
+      lat: Number(i.latitude),
+      lng: Number(i.longitude),
+      alt: Number(i.altitude),
+    });
+  }
+  let res = await axios.post("http://3.139.94.118:8080/send-command", {
+    command,
+  });
+};
 const updatePosition = async () => {
-  let res = await axios.get("http://localhost:8080/get-current-position");
+  let res = await axios.get("http://3.139.94.118:8080/get-current-position");
 
   current.latitude = res.data[0].latitude;
   current.longitude = res.data[0].longitude;
   current.altitude = res.data[0].altitude;
+
+  form.positions[0].latitude = res.data[0].latitude;
+  form.positions[0].longitude = res.data[0].longitude;
+  form.positions[0].altitude = res.data[0].altitude;
+
+  location.lat = Number(res.data[0].latitude);
+  location.lng = Number(res.data[0].longitude);
   timestamp.value = res.data[0].timestamp;
 };
 onMounted(() => {
