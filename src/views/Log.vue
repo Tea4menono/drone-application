b61cde98495a57531801f6b14f1c9c208cd09337
helloloop <1204386811@qq.com>
2023-06-26 16:28:14 -0600

    123

--- src/views/Log.vue
+++ src/views/Log.vue
@@ -1,17 +1,33 @@
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="Drone" name="first">Drone</el-tab-pane>
-    <el-tab-pane label="Server" name="second">Server</el-tab-pane>
+  <el-tabs v-model="activeName" class="demo-tabs">
+    <el-tab-pane label="Drone" name="first">
+      <div v-html="droneLog"></div>
+    </el-tab-pane>
+    <el-tab-pane label="Server" name="second">
+      <div v-html="AWSLog"></div
+    ></el-tab-pane>
   </el-tabs>
 </template>
 <script lang="ts" setup>
-import { ref } from "vue";
-import type { TabsPaneContext } from "element-plus";
-
+import { onMounted, ref, computed } from "vue";
+import axios from "axios";
 const activeName = ref("first");
+const droneLog = ref("");
+const AWSLog = ref("");
 
-const handleClick = (tab: TabsPaneContext, event: Event) => {
-  console.log(tab, event);
+const getAWSLog = async () => {
+  let res = await axios.get("http://3.139.94.118:8080/get-aws-log");
+
+  AWSLog.value = res.data.replace(/\n/g, "<br/>");
 };
+const getDroneLog = async () => {
+  let res = await axios.get("http://3.139.94.118:8080/get-drone-log");
+
+  droneLog.value = res.data.replace(/\n/g, "<br/>");
+};
+onMounted(() => {
+  getDroneLog();
+  getAWSLog();
+});
 </script>
 <style></style>
