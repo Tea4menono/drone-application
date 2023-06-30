080f92ffc5b93cd0881a090161548b34d5d59b51
helloloop <1204386811@qq.com>
2023-06-30 17:14:59 -0600

    123

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div class="container">
     <div>
       <el-menu class="el-menu-vertical-demo">
-        <RouterLink to="/">
+        <RouterLink to="/map">
           <el-menu-item index="1">
             <el-icon><Menu /></el-icon>
-            <template #title>Dashboard</template>
+            <template #title>Map</template>
           </el-menu-item>
         </RouterLink>
         <RouterLink to="/log">
@@ -20,13 +20,108 @@
     <div class="content">
       <RouterView />
     </div>
+
+    <el-dialog v-model="dialogVisible" title="Connection" width="30%" center>
+      <el-table
+        ref="singleTableRef"
+        :data="connections"
+        highlight-current-row
+        style="width: 100%"
+        @current-change="handleCurrentChange"
+      >
+        <el-table-column
+          property="connection"
+          label="Connection"
+          header-align="center"
+          align="center"
+        />
+
+        <el-table-column
+          label="Status"
+          header-align="center"
+          align="center"
+          width="100"
+        >
+          <template #default="scope">
+            <div v-if="scope.row.status == true" class="connectionTableCell">
+              Active<el-icon size="20" color="#67C23A"><CircleCheck /></el-icon>
+            </div>
+            <div v-else class="connectionTableCell">
+              Inactive<el-icon size="20" color="#F56C6C"
+                ><CircleClose
+              /></el-icon>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button
+            type="primary"
+            @click="dialogVisible = false"
+            :disabled="!currentRow || (currentRow && !currentRow.status)"
+          >
+            Confirm
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { onMounted, ref } from "vue";
 import { RouterLink, RouterView } from "vue-router";
+// import SerialPort from "serialport";
+import {
+  Menu,
+  Setting,
+  CircleCheck,
+  CircleClose,
+} from "@element-plus/icons-vue";
+import axios from "axios";
 
-import { Menu, Setting } from "@element-plus/icons-vue";
+const dialogVisible = ref(false);
+const currentRow = ref();
+const get4GStatus = async () => {
+  let res = await axios.get("http://3.139.94.118:8080/get-current-position");
+  let difference =
+    new Date().getTime() - new Date(res.data.timestamp).getTime();
+
+  connections.value[0].status = difference >= 0 && difference <= 5000;
+};
+
+const getSerialStatus = () => {
+  // const port = new SerialPort({
+  //   path: "/dev/tty.usbserial-D30EZNZF",
+  //   baudRate: 57600,
+  // });
+  connections.value[1].status = true;
+};
+onMounted(() => {
+  dialogVisible.value = true;
+  get4GStatus();
+  // getSerialStatus();
+});
+interface Connection {
+  connection: string;
+  status: Boolean;
+}
+
+const handleCurrentChange = (val: Connection | undefined) => {
+  currentRow.value = val;
+};
+
+const connections = ref([
+  {
+    connection: "4G LTE Modem",
+    status: false,
+  },
+  {
+    connection: "Telemetry Radio",
+    status: true,
+  },
+]);
 </script>
 
 <style scoped>
@@ -42,5 +137,11 @@ import { Menu, Setting } from "@element-plus/icons-vue";
 .content {
   height: 100vh;
   flex: 1;
+}
+
+.connectionTableCell {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
+import Map from "../views/Map.vue";
 import Log from "../views/Log.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "dashboard",
-      component: Dashboard,
+      path: "/map",
+      name: "map",
+      component: Map,
     },
     {
       path: "/log",
